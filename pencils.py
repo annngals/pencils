b2be6bf08c54151575f9b7bf2696e990eddae41e
annngals <57696371+annngals@users.noreply.github.com>
2020-11-06 21:34:53 +0800

    Update pencils.py

--- pencils.py
+++ pencils.py
@@ -7,10 +7,9 @@ Created on Tue Nov  3 12:33:09 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-from radius import circularity_std
 from scipy.ndimage import morphology
 from skimage.measure import label, regionprops
-from skimage.filters import threshold_triangle, threshold_otsu
+from skimage.filters import threshold_triangle
 
 
 def toGray(image):
@@ -26,43 +25,48 @@ def binarisation(image, limit_min, limit_max):
 def circularity(region, label = 1):
     return (region.perimeter**2) /region.area
 
-image = plt.imread("pencils/img (12).jpg")
-gray = toGray(image)
+pic = 1
+pencils = 0
 
-thresh = threshold_triangle(gray)
-binary = binarisation(gray, 0, thresh)
-
-binary = morphology.binary_dilation(binary, iterations = 1)
-
-labeled = label(binary)
-
-areas = []
-
-for region in regionprops(labeled):
-    areas.append(region.area)
-
-for region in regionprops(labeled):
-    if region.area < np.mean(areas):
-        labeled[labeled == region.label] = 0
-    bbox = region.bbox
-    if bbox[0] == 0 or bbox[1] == 0:
-        labeled[labeled == region.label] = 0
-
-labeled[labeled > 0] = 1
-labeled = label(labeled)
-
-n = 1
-pen = 0
-
-for region in regionprops(labeled):
-    if (((circularity(region, n) > 100) and (500000 > region.area > 330000))):
-        pen += 1
-    n += 1
+for pic in range(1, 13):
+    image = plt.imread("pencils/img ("+str(pic)+").jpg")
+    gray = toGray(image)
     
-#binary = morphology.binary_erosion(binary, iterations = 10)
-#binary = morphology.binary_dilation(binary, iterations = 40)
+    thresh = threshold_triangle(gray)
+    binary = binarisation(gray, 0, thresh)
+    
+    binary = morphology.binary_dilation(binary, iterations = 1)
+    
+    labeled = label(binary)
+    
+    areas = []
+    
+    for region in regionprops(labeled):
+        areas.append(region.area)
+    
+    for region in regionprops(labeled):
+        if region.area < np.mean(areas):
+            labeled[labeled == region.label] = 0
+        bbox = region.bbox
+        if bbox[0] == 0 or bbox[1] == 0:
+            labeled[labeled == region.label] = 0
+    
+    labeled[labeled > 0] = 1
+    labeled = label(labeled)
+    
+    n = 1
+    pen = 0
+    
+    for region in regionprops(labeled):
+        if (((circularity(region, n) > 100) and (500000 > region.area > 330000))):
+            pen += 1
+        n += 1
+        
+    print(pic, " ", pen)
+    pencils += pen
+    
 
-print("Pencils: ", pen)
+print("Pencils: ", pencils)
 
 plt.subplot(131)
 plt.imshow(gray, cmap="gray")
